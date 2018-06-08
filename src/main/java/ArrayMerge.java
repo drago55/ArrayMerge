e5ecf407efceca088f51010cd42c4b0bad74688b
Dragutin Horvat <dhorvat22@gmail.com>
2018-06-09 00:13:55 +0100

    added null check

--- src/main/java/ArrayMerge.java
+++ src/main/java/ArrayMerge.java
@@ -3,8 +3,33 @@ public class ArrayMerge {
 
     public int[] merge(int[] firstArray, int[] secondArray) {
 
-        int
+        int firstArrayLength = firstArray != null ? firstArray.length : 0;
+        int secondArrayLength = secondArray != null ? secondArray.length : 0;
+        int totalLength = firstArrayLength + secondArrayLength;
+        int[] mergedArray = new int[totalLength];
 
-        return null;
+        for (int i = 0; i < firstArrayLength; i++) {
+            mergedArray[i] = firstArray[i];
+            System.out.println("adding first array " + firstArray[i]);
+        }
+        int newLength = firstArrayLength;
+
+        for (int i = 0; i < secondArrayLength; i++) {
+
+            mergedArray[newLength] = secondArray[i];
+            System.out.println("adding second array " + secondArray[i]);
+            newLength++;
+        }
+        return mergedArray;
+    }
+
+    public static void main(String[] args) {
+        ArrayMerge merge = new ArrayMerge();
+        int firstArray[] = {1, 2, 5, 6, 3, 4};
+        int secondArray[] = {6, 55, 22, 7, 22};
+        int[] result = merge.merge(firstArray, secondArray);
+        for (int i : result) {
+            System.out.println("result of array " + i);
+        }
     }
 }
--- src/test/java/ArrayMergeShould.java
+++ src/test/java/ArrayMergeShould.java
@@ -15,4 +15,81 @@ public class ArrayMergeShould {
         Assertions.assertArrayEquals(expectedArray,
                 arrayMerge.merge(firstArray, secondArray));
     }
+
+    @Test
+    public void have_new_length() {
+        //Given
+        ArrayMerge arrayMerge = new ArrayMerge();
+        //When
+        int firstArray[] = {1, 2, 5, 6, 3, 4};
+        int secondArray[] = {6, 55, 22, 7, 22};
+        int expectedLength = firstArray.length + secondArray.length;
+        //Then
+        Assertions.assertEquals(expectedLength, arrayMerge.merge(firstArray, secondArray).length);
+    }
+
+    @Test
+    public void maintain_elements_order() {
+        //Given
+        ArrayMerge arrayMerge = new ArrayMerge();
+        //When
+        int firstArray[] = {1, 2, 5, 6, 3, 4};
+        int secondArray[] = {6, 55, 22, 7, 22};
+        int expectedElement = 22;
+        int atIndex = 8;
+        Assertions.assertEquals(expectedElement, arrayMerge.merge(firstArray, secondArray)[atIndex]);
+    }
+
+    @Test
+    public void merge_arrays_with_single_element() {
+        //Given
+        ArrayMerge arrayMerge = new ArrayMerge();
+        //When
+        int firstArray[] = {6};
+        int secondArray[] = {6};
+        int expectedArray[] = {6, 6};
+        //Then
+        Assertions.assertArrayEquals(expectedArray,
+                arrayMerge.merge(firstArray, secondArray));
+    }
+
+    @Test
+    public void merge_arrays_with_one_array_empty() {
+        //Given
+        ArrayMerge arrayMerge = new ArrayMerge();
+        //When
+        int firstArray[] = {6};
+        int secondArray[] = {};
+        int expectedArray[] = {6};
+        //Then
+        Assertions.assertArrayEquals(expectedArray,
+                arrayMerge.merge(firstArray, secondArray));
+    }
+
+    @Test
+    public void merge_empty() {
+        //Given
+        ArrayMerge arrayMerge = new ArrayMerge();
+        //When
+        int firstArray[] = {};
+        int secondArray[] = {};
+        int expectedArray[] = {};
+        //Then
+        Assertions.assertArrayEquals(expectedArray,
+                arrayMerge.merge(firstArray, secondArray));
+    }
+
+    @Test
+    public void merge_null() {
+        //Given
+        ArrayMerge arrayMerge = new ArrayMerge();
+        //When
+        int firstArray[] = null;
+        int secondArray[] = null;
+        int expectedArray[] = {};
+        //Then
+        Assertions.assertArrayEquals(expectedArray,
+                arrayMerge.merge(firstArray, secondArray));
+    }
+
 }
